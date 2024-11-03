0bde12ca80617c1034f88f4ce743df2275a88de6
demos-prog <demosne@gmail.com>
2024-11-03 17:39:01 +0300

    fav: albums

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -14,12 +14,14 @@ import { FavsService } from './favs.service';
 import { Favorites } from './entities/favorites.entity';
 import { TrackService } from 'src/track/track.service';
 import { currentUserId } from 'src/user/user.controller';
+import { AlbumService } from 'src/album/album.service';
 
 @Controller('favs')
 export class FavsController {
 	constructor(
 		private readonly favsService: FavsService,
 		private readonly trackService: TrackService,
+		private readonly albumService: AlbumService,
 	) {}
 
 	@Get()
@@ -28,7 +30,7 @@ export class FavsController {
 	}
 
 	@Post('track/:id')
-	async create(@Param('id', new ParseUUIDPipe()) trackId: string) {
+	async addToFavTracks(@Param('id', new ParseUUIDPipe()) trackId: string) {
 		const track = await this.trackService.findOne(trackId);
 		if (!track) {
 			throw new UnprocessableEntityException(
@@ -57,7 +59,7 @@ export class FavsController {
 
 	@Delete('track/:id')
 	@HttpCode(204)
-	async remove(@Param('id', new ParseUUIDPipe()) trackId: string) {
+	async removeFromFavTracks(@Param('id', new ParseUUIDPipe()) trackId: string) {
 		const favoritesOfUser: Favorites = await this.favsService.findOne(
 			currentUserId,
 		);
@@ -67,6 +69,51 @@ export class FavsController {
 		}
 		favoritesOfUser.tracks = favoritesOfUser.tracks.filter(
 			(id) => id !== trackId,
+		);
+
+		this.favsService.update(currentUserId, favoritesOfUser);
+	}
+
+	@Post('album/:id')
+	async addToFavAlbums(@Param('id', new ParseUUIDPipe()) albumId: string) {
+		const album = await this.albumService.findOne(albumId);
+		if (!album) {
+			throw new UnprocessableEntityException(
+				`Track with ID ${albumId} not found`,
+			);
+		}
+		const favoritesOfUser: Favorites = await this.favsService.findOne(
+			currentUserId,
+		);
+		if (!favoritesOfUser) {
+			const newFavorites: Favorites = {
+				albums: [albumId],
+				artists: [],
+				tracks: [],
+			};
+			return this.favsService.create(currentUserId, newFavorites);
+		}
+		if (favoritesOfUser.tracks.includes(albumId)) {
+			throw new ForbiddenException(
+				`Track with ID ${albumId} is already in favorites`,
+			);
+		}
+		favoritesOfUser.albums.push(albumId);
+		return this.favsService.update(currentUserId, favoritesOfUser);
+	}
+
+	@Delete('album/:id')
+	@HttpCode(204)
+	async removeFromFavAlbums(@Param('id', new ParseUUIDPipe()) albumId: string) {
+		const favoritesOfUser: Favorites = await this.favsService.findOne(
+			currentUserId,
+		);
+		const album = favoritesOfUser.albums.find((id) => id === albumId);
+		if (!album) {
+			throw new NotFoundException(`Track with ID ${albumId} not found`);
+		}
+		favoritesOfUser.albums = favoritesOfUser.albums.filter(
+			(id) => id !== albumId,
 		);
 
 		this.favsService.update(currentUserId, favoritesOfUser);
--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -3,10 +3,11 @@ import { FavsService } from './favs.service';
 import { FavsController } from './favs.controller';
 import { PrismaModule } from 'src/prisma.module';
 import { TrackService } from 'src/track/track.service';
+import { AlbumService } from 'src/album/album.service';
 
 @Module({
 	imports: [PrismaModule],
 	controllers: [FavsController],
-	providers: [FavsService, TrackService],
+	providers: [FavsService, TrackService, AlbumService],
 })
 export class FavsModule {}
